e10d9a1ba8cb32e372e49108fb39b2841d9756fc
Johannes Lindeman <johannes.lindeman@tuni.fi>
2021-10-28 13:25:33 +0300

    ...

---
+++ index.js
@@ -0,0 +1,19 @@
+/*var readlineSync = require('readline-sync')
+
+var userName = readlineSync.question("May I have Your Name?");
+console.log("Hi " + userName + "!");*/
+
+
+
+/*for(let i=0; i<10; i++){
+    console.log(i);
+}*/
+
+let x = 5;
+
+console.log(x);
+console.log(typeof x);
+
+let jaska = { nimi: "jaska", ika: 30};
+
+console.log(jaska);
