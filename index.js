68f1b5f582332ec874e4d053db5e1b5a3e019704
Johannes Lindeman <johannes.lindeman@tuni.fi>
2021-11-14 19:47:50 +0200

    backend fix for routing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
 
 app.use(express.static("public"));
 //app.use(express.static("public"));
@@ -9,21 +10,21 @@ app.use(express.static("public"));
 //  res.send("Hello world");
 //});
 
-app.get("/Search", (req, res) => {
+/*app.get("/Search", (req, res) => {
   res.sendFile(path.join("public/index.html"));
 });
 app.get("/HomePage", (req, res) => {
   res.sendFile(path.join("public/index.html"));
-});
+});*/
 
-/*app.get("/*", function (req, res) {
-  res.sendFile(path.join("__dirname", "/public/index.html"), function (err) {
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"), function (err) {
     if (err) {
       res.status(500).send(err);
     }
   });
 });
-*/
+
 app.listen(port, () => {
   console.log("Example app listening at http://localhost:" + port);
 });
