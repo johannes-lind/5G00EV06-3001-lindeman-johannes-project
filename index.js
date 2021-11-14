dd256512a0c20555be11ed651762603b9c603914
Johannes Lindeman <johannes.lindeman@tuni.fi>
2021-11-14 13:14:31 +0200

    ...

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ app.get("/HomePage", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/*", function (req, res) {
+/*app.get("/*", function (req, res) {
   res.sendFile(path.join("__dirname", "/public/index.html"), function (err) {
     if (err) {
       res.status(500).send(err);
     }
   });
 });
-
+*/
 app.listen(port, () => {
   console.log("Example app listening at http://localhost:" + port);
 });
